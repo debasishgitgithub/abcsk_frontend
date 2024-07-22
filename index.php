884c515145c413d9c896a7c211ca72fea65fc042
debasish <ddas1112000@gmail.com>
2024-07-22 20:50:45 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+require 'layout/header.php';
+?>
+<main class="main">
+
+  <!-- Hero Section -->
+  <section id="hero" class="hero section">
+
+    <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+
+    <div class="container">
+      <h2 data-aos="fade-up" data-aos-delay="100" class="">Learning Today,<br>Leading Yesterday</h2>
+      <p data-aos="fade-up" data-aos-delay="200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, iure!</p>
+      <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
+        <!-- <a href="test.php" class="btn-get-started">Get Started</a> -->
+      </div>
+    </div>
+
+  </section><!-- /Hero Section -->
+
+  <!-- About Section -->
+  <section id="about" class="about section">
+
+    <div class="container">
+
+      <div class="row gy-4">
+
+        <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="100">
+          <img src="assets/img/about.jpg" class="img-fluid" alt="">
+        </div>
+
+        <div class="col-lg-6 order-2 order-lg-1 content" data-aos="fade-up" data-aos-delay="200">
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, similique.</h3>
+          <p class="fst-italic">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span></li>
+            <li><i class="bi bi-check-circle"></i> <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span></li>
+          </ul>
+          <!-- <a href="#" class="read-more"><span>Read More</span><i class="bi bi-arrow-right"></i></a> -->
+        </div>
+
+      </div>
+
+      
+    </div>
+
+  </section><!-- /About Section -->
+
+  <!-- Counts Section -->
+  <section id="counts" class="section counts">
+
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+      <div class="row gy-4">
+
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="1232" data-purecounter-duration="1" class="purecounter"></span>
+            <p class="">Students</p>
+          </div>
+        </div><!-- End Stats Item -->
+
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" class="purecounter"></span>
+            <p class="">Courses</p>
+          </div>
+        </div><!-- End Stats Item -->
+
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1" class="purecounter"></span>
+            <p class="">Events</p>
+          </div>
+        </div><!-- End Stats Item -->
+
+        <div class="col-lg-3 col-md-6">
+          <div class="stats-item text-center w-100 h-100">
+            <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="1" class="purecounter"></span>
+            <p class="">Trainers</p>
+          </div>
+        </div><!-- End Stats Item -->
+
+      </div>
+
+    </div>
+
+  </section><!-- /Counts Section -->
+
+  <!-- Why Us Section -->
+  <section id="my-section" class="section why-us">
+
+  </section><!-- /Why Us Section -->
+
+</main>
+
+<?php
+require 'layout/footer.php';
+?>
